9cfb562857cabc360704f0987460ff606d154531
shivajee98 <ultimateintimater@gmail.com>
2025-04-11 12:31:39 +0530

    clerk error resolved

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+export const dynamic = "force-dynamic"
 const AuthenticationLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className='w-full min-h-screen flex items-center justify-center'>
